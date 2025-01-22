9ca1f22559651c1c5a8ce1858d832d62e2b11479
adela-addamsova <a.addamsova@gmail.com>
2025-01-22 12:30:17 +0100

    feat: add router for gallery pages

--- app/Core/RouterFactory.php
+++ app/Core/RouterFactory.php
@@ -12,10 +12,9 @@ final class RouterFactory
 {
 	use Nette\StaticClass;
 
-	public static function createRouter(): RouteList
-	{
-		$router = new RouteList;
-		$router->addRoute('<presenter>/<action>[/<id>]', 'Home:default');
-		return $router;
+	public static function createRouter(): RouteList { $router = new RouteList; // Add a route for the gallery with a category parameter 
+		$router->addRoute('gallery/<category>', 'Gallery:default');
+
+		$router->addRoute('<presenter>/<action>[/<id>]', 'Home:default'); return $router;
 	}
 }
