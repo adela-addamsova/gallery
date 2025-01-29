3dd496814258a5aa0b09343c6c7d9d6812356046
adela-addamsova <a.addamsova@gmail.com>
2025-01-29 17:08:58 +0100

    refactor: include js code for delete confirmation in delete.js file

---
+++ www/js/delete.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    if (localStorage.getItem("flashMessage")) {
+        let flashMessage = localStorage.getItem("flashMessage");
+
+        document.getElementById("flash-container").innerHTML =
+            '<div class="alert alert-success mx-auto" role="alert">' +
+            flashMessage +
+            "</div>";
+
+        localStorage.removeItem("flashMessage");
+    }
+
+    $(".delete").on("click", function () {
+        event.preventDefault();
+        let confirmMessage = $(this).data("message");
+
+        if (confirm(confirmMessage)) {
+            let url = $(this).attr("href");
+
+            $.ajax({
+                url: url,
+                type: "POST",
+                success: function (data) {
+                    if (data.redirect) {
+                        localStorage.setItem("flashMessage", data.flashMessage);
+
+                        window.location.href = data.redirect;
+                    }
+                },
+            });
+        }
+    });
+});
