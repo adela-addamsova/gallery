4160c3c5b40e8c1ca9714e92fc9f37398466de6b
adela-addamsova <a.addamsova@gmail.com>
2025-02-05 13:26:20 +0100

    feat: create a basic html template for email from contact form

--- app/Model/Facades/ContactFacade.php
+++ app/Model/Facades/ContactFacade.php
@@ -3,11 +3,16 @@ declare(strict_types=1);
 
 namespace App\Model\Facades;
 
+use Nette\Bridges\ApplicationLatte\LatteFactory;
 use Nette\Mail\SmtpMailer;
 use Nette\Mail\Message;
 
 class ContactFacade
 {
+	public function __construct(
+		private LatteFactory $latteFactory,
+	) {
+	}
 	public function sendMessage(string $email, string $name, string $message): void
 	{
         $mailer = new SmtpMailer (
@@ -16,12 +21,19 @@ class ContactFacade
             password: '',
             encryption: 'ssl',
         );
+
+		$latte = $this->latteFactory->create();
+		$body = $latte->renderToString(__DIR__ . '/contactEmail.latte', [
+			'email' => $email,
+			'name' => $name,
+			'message' => $message,
+		]);
         
 		$mail = new Message;
 		$mail->addTo('adadaq@seznam.cz')
 			->setFrom($email, $name)
 			->setSubject('Message from the contact form')
-			->setBody($message);
+			->setHtmlBody($body);
 
 		$mailer->send($mail);
 	}
