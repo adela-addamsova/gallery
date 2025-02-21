bc606e1a750050783804a226d4f315568eafb645
adela-addamsova <a.addamsova@gmail.com>
2025-02-21 15:34:49 +0100

    feat: adjust photo form in admin mode - images are directly updated and to the form and are converted to webp, thumb is created automatically

--- app/Components/PhotoForm/PhotoForm.php
+++ app/Components/PhotoForm/PhotoForm.php
@@ -7,38 +7,24 @@ namespace App\Components\PhotoForm;
 use App\Model\Facades\AdminFacade;
 use Nette\Application\UI\Control;
 use Nette\Application\UI\Form;
+use Nette\Http\FileUpload;
+use Nette\Utils\ArrayHash;
 
 class PhotoForm extends Control
 {
-    // Event callback triggered when the form is saved
     public array $onSave = [];
-
-    // Edit mode flag to determine if we are editing an existing image or creating a new one
     private bool $isEditMode;
-
-    // Holds image data for editing when in edit mode
-    private $imageData = null;
+    private $imageData = null;  
 
     /** @var AdminFacade @inject */
     public $adminFacade;
 
-    /**
-     * Set the edit mode and optionally provides image data for editing
-     * 
-     * @param bool $isEditMode - Indicates if edit mode is set
-     * @param mixed $imageData - Image data to be used for editing
-     */
     public function setEditMode(bool $isEditMode, $imageData = null): void
     {
         $this->isEditMode = $isEditMode;
         $this->imageData = $imageData;
     }
 
-    /**
-     * Create the form component for photo data
-     * 
-     * @return Form - The generated form component
-     */
     protected function createComponentForm(): Form
     {
         $form = new Form;
@@ -48,21 +34,14 @@ class PhotoForm extends Control
         $form->addText('category_id', 'Category ID')
             ->setHtmlAttribute('placeholder', 'Category_ID')
             ->setRequired();
-        $form->addText('path', 'Full image path')
-            ->setHtmlAttribute('placeholder', '/img/macro/img1.webp')
-            ->setRequired();
-        $form->addText('thumb_path', 'Thumb path')
-            ->setHtmlAttribute('placeholder', '/img/macro/thumbs/img1.webp')
-            ->setRequired();
-
+        $form->addUpload('image', 'Image')
+            ->setRequired($this->isEditMode ? false : 'Please upload an image.');
         $form->addSubmit('send', $this->isEditMode ? 'Update' : 'Save');
 
         if ($this->isEditMode && $this->imageData) {
             $form->setDefaults([
                 'description' => $this->imageData['description'],
                 'category_id' => $this->imageData['category_id'],
-                'path' => $this->imageData['path'],
-                'thumb_path' => $this->imageData['thumb_path'],
             ]);
         }
 
@@ -71,22 +50,13 @@ class PhotoForm extends Control
         return $form;
     }
 
-    /**
-     * Handle the form submission when it is successfully processed
-     * 
-     * @param array $values - The submitted form data
-     */
-    public function processForm(array $values): void
+    public function processForm(Form $form, ArrayHash $values): void
     {
-        // Trigger all the onSave callbacks registered by the component or external code
         foreach ($this->onSave as $callback) {
             $callback($this, $values);
         }
     }
 
-    /**
-     * Render the PhotoForm component
-     */
     public function render(): void
     {
         $this->template->isEditMode = $this->isEditMode;
--- app/UI/Admin/AddPhoto/AddPhotoPresenter.php
+++ app/UI/Admin/AddPhoto/AddPhotoPresenter.php
@@ -8,6 +8,8 @@ use App\Components\PhotoForm\PhotoForm;
 use App\Model\Facades\AdminFacade;
 use App\Model\Interfaces\PhotoFormFactory;
 use App\UI\Admin\BasePresenter;
+use Exception;
+use Nette\Http\FileUpload;
 
 class AddPhotoPresenter extends BasePresenter
 {
@@ -23,6 +25,44 @@ class AddPhotoPresenter extends BasePresenter
     /** @var AdminFacade @inject */
     public $adminFacade;
 
+    function createThumbnail($sourceImagePath, $thumbnailPath, $thumbnailWidth = 1678, $thumbnailHeight = 944)
+    {
+        list($originalWidth, $originalHeight, $imageType) = getimagesize($sourceImagePath);
+
+        $thumbnail = imagecreatetruecolor($thumbnailWidth, $thumbnailHeight);
+
+        switch ($imageType) {
+            case IMAGETYPE_JPEG:
+                $sourceImage = imagecreatefromjpeg($sourceImagePath);
+                break;
+            case IMAGETYPE_PNG:
+                $sourceImage = imagecreatefrompng($sourceImagePath);
+                break;
+            case IMAGETYPE_GIF:
+                $sourceImage = imagecreatefromgif($sourceImagePath);
+                break;
+            case IMAGETYPE_WEBP:
+                $sourceImage = imagecreatefromwebp($sourceImagePath);
+                break;
+            default:
+                throw new Exception("Unsupported image type");
+        }
+
+        $webpPath = preg_replace('/\.[^.]+$/', '.webp', $sourceImagePath); 
+        imagewebp($sourceImage, $webpPath);
+
+        imagedestroy($sourceImage);
+
+        $sourceImage = imagecreatefromwebp($webpPath);
+
+        imagecopyresampled($thumbnail, $sourceImage, 0, 0, 0, 0, $thumbnailWidth, $thumbnailHeight, $originalWidth, $originalHeight);
+
+        imagewebp($thumbnail, $thumbnailPath);
+
+        imagedestroy($sourceImage);
+        imagedestroy($thumbnail);
+    }
+
     /**
      * Creates the photo form component
      * 
@@ -35,6 +75,41 @@ class AddPhotoPresenter extends BasePresenter
         $photoForm->setEditMode(false);
 
         $photoForm->onSave[] = function (PhotoForm $photoForm, $data): void {
+            /** @var FileUpload $image */
+            $image = $data->image;
+            $category = $this->adminFacade->getCategory($data->category_id);
+
+            $uploadDir = 'img/' . strtolower($category['name']) . '/';
+            $thumbnailDir = 'img/' . strtolower($category['name']) . '/thumbs/';
+
+            if (!file_exists($uploadDir)) {
+                mkdir($uploadDir, 0755, true);
+            }
+            if (!file_exists($thumbnailDir)) {
+                mkdir($thumbnailDir, 0755, true);
+            }
+
+            $filename = $image->getSanitizedName();
+            $filenameWebp = preg_replace('/\.[^.]+$/', '.webp', $filename);
+
+
+            $imagePath = $uploadDir . $filenameWebp;
+            $thumbnailPath = $thumbnailDir . $filenameWebp;
+
+            $tempPath = $uploadDir . $filename;
+            $image->move($tempPath);
+
+            $this->createThumbnail($tempPath, $thumbnailPath);
+
+            unlink($tempPath);
+
+            $data = [
+                'description' => $data->description,
+                'category_id' => $data->category_id,
+                'path' => $uploadDir . $filenameWebp,
+                'thumb_path' => $thumbnailDir . $filenameWebp,
+            ];
+
             $this->adminFacade->insertImage($data);
             $this->flashMessage('Photo was added successfully!', 'success');
         };
